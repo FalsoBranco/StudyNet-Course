cf8672fdeae1b2df70fe71396615d7ce3eacb23a
helter daniel <helterdaniel@hotmail.com>
2021-12-30 00:20:40 -0300

    course admin

--- backend/course/admin.py
+++ backend/course/admin.py
@@ -1,35 +1,91 @@
-from django.contrib import admin
+from typing import Any, Dict, List, Optional, Tuple
 
-from .models import Category, Comment, Course, Lesson
+from django.contrib import admin
+from django.db.models.fields.related import RelatedField
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
+
+from course.models import Category, Comment, Course, Lesson
 
 # Register your models here.
 
 
-class CommentsInline(admin.StackedInline):
-    model = Comment
-    raw_id_fields = ["lesson"]
-
-
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ("slug",)
+    filter_horizontal = ("categories",)
+
+    # Admin Options
+    list_per_page = 13
+    list_display = (
+        "title",
+        "slug",
+        "created_at",
+        "updated_at",
+        "is_active",
+    )
+    search_fields = ("title",)
+    list_filter = ("categories", "is_active")
+    list_editable = ("is_active",)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ("slug",)
+
+    # Admin Options
+    list_per_page = 13
+    list_editable = ("is_active",)
+    list_display = (
+        "title",
+        "slug",
+        "created_at",
+        "updated_at",
+        "is_active",
+    )
+    search_fields = ("title",)
+    list_filter = ("is_active",)
 
 
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ["title", "course", "status", "lesson_type"]
-    list_filter = ["status", "lesson_type"]
-    search_fields = ["title", "short_description", "long_description"]
-    inlines = [
-        CommentsInline,
-    ]
+    readonly_fields = ("slug",)
+    # Admin Options
+    list_per_page = 13
+    list_editable = ("is_active",)
+    list_display = (
+        "title",
+        "slug",
+        "course",
+        "status",
+        "lesson_type",
+        "is_active",
+    )
+    list_filter = (
+        "status",
+        "lesson_type",
+        "is_active",
+    )
+    search_fields = (
+        "title",
+        "short_description",
+        "long_description",
+    )
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    pass
+
+    # Admin Options
+    list_per_page = 13
+    list_display = (
+        "course",
+        "lesson",
+        "name",
+        "created_by",
+        "created_at",
+        "updated_at",
+        "is_active",
+    )
+    search_fields = ("course", "lesson", "name")
+    list_filter = ("is_active",)
