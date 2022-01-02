ddbe7ae2de7b8e1186e7b484dc8847c134370472
helter daniel <helterdaniel@hotmail.com>
2022-01-02 20:09:30 -0300

    inline serializer class

--- backend/commons/utils.py
+++ backend/commons/utils.py
@@ -1,10 +1,15 @@
 import random
 import string
+from typing import Any, Dict
 
 from django.utils.text import slugify
+from rest_framework import serializers
 
 
-def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
+def random_string_generator(
+    size: int = 10,
+    chars: str = string.ascii_lowercase + string.digits,
+) -> str:
     return "".join(random.choice(chars) for _ in range(size))
 
 
@@ -24,3 +29,15 @@ def unique_slug_generator(sender, instance, new_slug=None):
 
         return unique_slug_generator(sender, instance, new_slug=new_slug)
     return slug
+
+
+def create_serializer_class(name: str, fields: Dict[str, Any]):
+    return type(name, (serializers.Serializer,), fields)
+
+
+def inline_serializer(*, fields: Dict[str, Any], data=None, **kwargs):
+    serializer_class = create_serializer_class(name="", fields=fields)
+
+    if data is not None:
+        return serializer_class(data=data, **kwargs)
+    return serializer_class(**kwargs)
