69d594a8141ee368d30d02d09b74ef4cb7283585
helter daniel <helterdaniel@hotmail.com>
2022-01-04 23:41:22 -0300

    axion config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,19 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-
 import router from './router'
-import axios from 'axios'
 
-import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 
 const app = createApp(App)
 
 app.config.productionTip = true
 
-axios.defaults.baseURL = 'http://localhost:8000'
-
 app.use(createPinia())
-app.use(router, axios, Buefy)
+app.use(router)
 app.mount('#app')
